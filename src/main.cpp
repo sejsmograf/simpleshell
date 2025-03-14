f9289135ce534df85084021379e7eb60563e35a4
Tomasz Mroczko <tomekmr2001@gmail.com>
2025-03-14 13:20:20 +0100

    sejsmo

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <string>
+
+void handle_input(const std::string &input) {
+  std::cout << "Command not found";
+}
+
+void print_prompt() { std::cout << "$ "; }
+
+void main_loop() {
+  print_prompt();
+  std::string input;
+  std::getline(std::cin, input);
+  handle_input(input);
+}
 
 void setup() {
   std::cout << std::unitbuf; // Enable automatic fluhsing after any output
